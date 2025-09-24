4543ce323799dd8d7a48cc5143b4625772585201
Mihail Tumanov <tumanov.mike@yandex.ru>
2025-09-25 01:45:38 +0400

    Add project files.

---
+++ Practicum_5/main.cpp
@@ -0,0 +1,167 @@
+#include "Windows.h"
+
+//стурктура где храняться данные о windows окне
+struct
+{
+	//дескрипторы, контейнеры и буфферы для windows
+	RECT rc;
+	HINSTANCE hIns;
+	HWND hWnd;
+	HDC dev_cont, contx;
+	MSG msg;
+	BOOL gbool = true;
+
+	//определяет размер экрана в вашей сиситеме
+	int width = GetSystemMetrics(SM_CXSCREEN), height = GetSystemMetrics(SM_CYSCREEN);
+} window;
+
+//обработка потока сообщений
+static LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+};
+
+//создания windows окна
+void InitWindow()
+{
+	//имя класса окна
+	const char* NameClass = "Window";
+
+	//размер окна
+	window.rc = { 0,0,window.width,window.height
+	};
+
+	//учет размера
+	AdjustWindowRect(&window.rc, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE);
+
+	//дескриптор класса окна
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.lpszClassName = NameClass;
+	wc.hInstance = window.hIns;
+	wc.lpfnWndProc = &WindowProc;
+
+	//регистрация класса окна
+	auto NameClassId = RegisterClassEx(&wc);
+
+	//деструктор окна
+	window.hWnd = CreateWindowEx(
+		NULL,
+		MAKEINTATOM(NameClassId),
+		"practicum5",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		window.rc.right - window.rc.left,
+		window.rc.bottom - window.rc.top,
+		NULL,
+		NULL,
+		window.hIns,
+		NULL
+	);
+
+	//показ окна
+	ShowWindow(window.hWnd, SW_SHOW);
+}
+
+//отрисовка изображений .bmp
+void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall)
+{
+	HBITMAP hbm, hOldbm;
+	HDC hMemDC;
+	BITMAP bm;
+
+	hMemDC = CreateCompatibleDC(hDC);
+	hOldbm = (HBITMAP)SelectObject(hMemDC, hBitmapBall);
+
+	if (hOldbm)
+	{
+		GetObject(hBitmapBall, sizeof(BITMAP), (LPSTR)&bm);
+		StretchBlt(hDC, x, y, x1, y1, hMemDC, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
+		SelectObject(hMemDC, hOldbm);
+	}
+
+	DeleteDC(hMemDC);
+}
+
+//загрузка модулей приложения
+void InitApp()
+{
+	//создание и иниализация контекста устройсва и девайс устройства
+	window.dev_cont = GetDC(window.hWnd);
+	window.contx = CreateCompatibleDC(window.dev_cont);
+	SelectObject(window.contx, CreateCompatibleBitmap(window.dev_cont, window.width, window.height));
+}
+
+//обновление приложения
+void UpdateApp()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+//обработка команд устройств ввода
+void UpdateKeyCode()
+{
+	//выход из приложения на ESC
+	if (GetAsyncKeyState(VK_ESCAPE))
+	{
+		window.msg.message = WM_QUIT;
+	}
+}
+
+//вход в программу
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd)
+{
+	InitWindow();
+	InitApp();
+
+	//основной цикл обновления приложения
+	while (window.gbool)
+	{
+		//обработка соощений для окна
+		while (PeekMessage(&window.msg, NULL, 0, 0, PM_REMOVE))
+		{
+			UpdateKeyCode();
+
+			//отбработка сообщений
+			if (window.msg.message == WM_QUIT)
+			{
+				window.gbool = false;
+				break;
+			}
+			TranslateMessage(&window.msg);
+			DispatchMessage(&window.msg);
+		}
+
+		UpdateApp();
+
+		//задержка обновления
+		Sleep(16);
+	}
+	return 0;
+}
