d45fc6b2de57cf021a1901e730ce1a58a4743f47
Mihail Tumanov <tumanov.mike@yandex.ru>
2025-10-06 02:40:16 +0400

    Брезенхэйм

--- Practicum_5/main.cpp
+++ Practicum_5/main.cpp
@@ -1,10 +1,11 @@
 #include "Windows.h"
 #include "math.h"
 
-//стурктура где храняться данные о windows окне
+/** стурктура где храняться данные о windows окне
+*/
 struct
 {
-	//дескрипторы, контейнеры и буфферы для windows
+	/// дескрипторы, контейнеры и буфферы для windows
 	RECT rc;
 	HINSTANCE hIns;
 	HWND hWnd;
@@ -12,17 +13,22 @@ struct
 	MSG msg;
 	BOOL gbool = true;
 
-	//определяет размер экрана в вашей сиситеме
-	int width = GetSystemMetrics(SM_CXSCREEN), height = GetSystemMetrics(SM_CYSCREEN);
+	int width = GetSystemMetrics(SM_CXSCREEN), height = GetSystemMetrics(SM_CYSCREEN); /// определяет размер экрана в вашей сиситеме
 } window;
 
-//структура с данными о координатах
+/** структура с данными о координатах
+*/
 struct
 {
-	float x, y, startX, startY, dx, dy;
+	int dx, 
+		dy,
+		signX,
+		signY,
+		direction;
 } Transform;
 
-//обработка потока сообщений
+/** обработка потока сообщений
+*/
 static LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
@@ -35,31 +41,25 @@ static LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lP
 	}
 };
 
-//создания windows окна
+/** создания windows окна
+*/
 void InitWindow()
 {
-	//имя класса окна
-	const char* NameClass = "Window";
+	const char* NameClass = "Window"; /// имя класса окна
 
-	//размер окна
-	window.rc = { 0,0,window.width,window.height
-	};
+	window.rc = { 0,0,window.width,window.height}; /// размер окна
 
-	//учет размера
-	AdjustWindowRect(&window.rc, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE);
+	AdjustWindowRect(&window.rc, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE); /// учет размера
 
-	//дескриптор класса окна
-	WNDCLASSEX wc = { 0 };
+	WNDCLASSEX wc = { 0 }; /// дескриптор класса окна
 	wc.cbSize = sizeof(wc);
 	wc.lpszClassName = NameClass;
 	wc.hInstance = window.hIns;
 	wc.lpfnWndProc = &WindowProc;
 
-	//регистрация класса окна
-	auto NameClassId = RegisterClassEx(&wc);
+	auto NameClassId = RegisterClassEx(&wc); /// регистрация класса окна
 
-	//деструктор окна
-	window.hWnd = CreateWindowEx(
+	window.hWnd = CreateWindowEx( /// деструктор окна
 		NULL,
 		MAKEINTATOM(NameClassId),
 		"practicum5",
@@ -74,12 +74,12 @@ void InitWindow()
 		NULL
 	);
 
-	//показ окна
-	ShowWindow(window.hWnd, SW_SHOW);
+	ShowWindow(window.hWnd, SW_SHOW); /// показ окна
 }
 
-//отрисовка изображений .bmp
-void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall)
+/** отрисовка изображений .bmp
+*/
+void ShowBitmap(HDC hDC, int x, int y, int x0, int y0, HBITMAP hBitmapBall)
 {
 	HBITMAP hbm, hOldbm;
 	HDC hMemDC;
@@ -91,52 +91,72 @@ void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall)
 	if (hOldbm)
 	{
 		GetObject(hBitmapBall, sizeof(BITMAP), (LPSTR)&bm);
-		StretchBlt(hDC, x, y, x1, y1, hMemDC, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
+		StretchBlt(hDC, x, y, x0, y0, hMemDC, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
 		SelectObject(hMemDC, hOldbm);
 	}
 
 	DeleteDC(hMemDC);
 }
 
-//загрузка модулей приложения
+/** загрузка модулей приложения
+*/
 void InitApp()
 {
-	//создание и иниализация контекста устройсва и девайс устройства
+	/// создание и иниализация контекста устройсва и девайс устройства
 	window.dev_cont = GetDC(window.hWnd);
 	window.contx = CreateCompatibleDC(window.dev_cont);
 	SelectObject(window.contx, CreateCompatibleBitmap(window.dev_cont, window.width, window.height));
 }
 
-//обновление приложения
-void UpdateApp()
+/** Функция отрисвоки линии на экране окна по Брезенхэйму
+*/
+void DrawLine(int x0, int y0, int x1, int y1)
 {
-	//центры экрана по ширине и высоте и так же стартовые точки начала рисования линий
-	Transform.startX = window.width / 2;
-	Transform.startY = window.height / 2; 
+	/// Вычисление дельт
+	Transform.dx = abs(x1 - x0);
+	Transform.dy = abs(y1 - y0);
 
-	//точки куда нужно нарисовать линию
-	Transform.x = 1600;
-	Transform.y = 400;
+	if (x0 < x1)
+		Transform.signX = 1;
+	else
+		Transform.signX = -1;
 
-	//дельты координат
-	Transform.dx = Transform.x - Transform.startX;
-	Transform.dy = Transform.y - Transform.startY;
+	if (y0 < y1)
+		Transform.signY = 1;
+	else
+		Transform.signY = -1;
 
-	//определение длины гипотенузы по катитам x, y по теореме пифагора
-	int length = sqrt(pow(Transform.dx, 2) + pow(Transform.dy, 2));
+	Transform.direction = Transform.dx - Transform.dy;
 
-	for (int i = 0; i < length; i++)
+	while (true)
 	{
-		//Вычисления шага отрисовки пикселей при помощи алгоритма Брезенхэма
-		int PixelPointX = Transform.dx * i / length + Transform.startX;
-		int PixelPointY = Transform.dy * i / length + Transform.startY;
+		SetPixel(window.contx, x0, y0, RGB(255, 0, 0));
 
-		//отрисовка пикселей на экране окна
-		SetPixel(window.contx, PixelPointX, PixelPointY, RGB(255, 0, 0));
+		if (Transform.direction > -Transform.dy)
+		{
+			Transform.direction -= Transform.dy;
+			x0 += Transform.signX;
+		}
+		if (Transform.direction < Transform.dx)
+		{
+			Transform.direction += Transform.dx;
+			y0 += Transform.signY;
+		}
+
+		if (x0 == x1 || y0 == y1)
+			break;
 	}
 }
 
-//обработка команд устройств ввода
+/** Обновление приложения
+*/
+void UpdateApp()
+{
+	DrawLine(100, 300, 1900, 200);
+}
+
+/** обработка команд устройств ввода
+*/
 void UpdateKeyCode()
 {
 	//выход из приложения на ESC
@@ -146,15 +166,16 @@ void UpdateKeyCode()
 	}
 }
 
-//обновление изображений
+/** обновление изображений
+*/
 void UpdateImage()
 {
 	BitBlt(window.dev_cont, 0, 0, window.width, window.height, window.contx, 0, 0, SRCCOPY);
-	//отрисовка заднего фона
-	ShowBitmap(window.contx, 0, 0, window.width, window.height, (HBITMAP)LoadImageA(NULL, "back.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE));
+	ShowBitmap(window.contx, 0, 0, window.width, window.height, (HBITMAP)LoadImageA(NULL, "back.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE)); /// отрисовка заднего фона
 }
 
-//вход в программу
+/** вход в программу
+*/
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -164,10 +185,10 @@ int CALLBACK WinMain(
 	InitWindow();
 	InitApp();
 
-	//основной цикл обновления приложения
+	/// основной цикл обновления приложения
 	while (window.gbool)
 	{
-		//обработка соощений для окна
+		/// обработка соощений для окна
 		while (PeekMessage(&window.msg, NULL, 0, 0, PM_REMOVE))
 		{
 			UpdateKeyCode();
@@ -185,8 +206,7 @@ int CALLBACK WinMain(
 		UpdateImage();
 		UpdateApp();
 
-		//задержка обновления
-		Sleep(16);
+		Sleep(16); /// задержка обновления кадров
 	}
 	return 0;
 }
