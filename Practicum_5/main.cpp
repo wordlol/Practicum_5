65a09756f111428bd4b729bc312f383812ca616d
Mihail Tumanov <tumanov.mike@yandex.ru>
2025-10-10 01:02:28 +0400

    интерполяции

--- Practicum_5/main.cpp
+++ Practicum_5/main.cpp
@@ -466,6 +466,7 @@ float FindZPoint3(float x1, float x2, float x3, float y1, float y2, float y3, fl
 		return z_target;
 }
 
+
 /** »нтерпол€ци€ Z координаты (построение плоскости)
 */
 float FindZPoint2(float x1, float x2, float x3, float y1, float y2, float y3,float z1,float z2,float z3, float x, float y)
